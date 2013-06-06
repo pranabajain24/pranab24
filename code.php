baf5fc3e27bca44e673c1195feacdd324fe406b2
pranabjain <pranabjain24@gmail.com>
2013-06-06 11:36:29 +0530

    first update

--- code.php
+++ code.php
@@ -1,13 +1,38 @@
 <html>
+<head>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script>
+
+function validateForm()
+{
+
+	var x=document.forms["form1"]["user_name"].value;
+	var y=document.forms["form1"]["mobile_number"].value;
+	if (x==null || x=="")
+  	{
+		alert("First name must be filled out");
+		return false;
+  	}
+	else if (y==null || y=="")
+	{
+		alert("First numder must be filled out");
+		return false;
+	}
+
+}
+
+</script>
+</head>
 <body>
 <title>hoppr</title>
 
+<p>Enter your name</p>
+<form name = "form1" onsubmit="return validateForm();" method= "POST" action = "code1.php">
+<input type = 'text' name='user_name' id="name" value=<?php print $user_name ; ?>>
 
-Enter your name<br>
-<form name = "form1" method= "POST" action = "code1.php">
-<input type = 'text' name='user_name' value=<?php print $user_name ; ?>>
-<br>Enter your mobile number<br>
-<input type = 'text' name ='mobile_number' value=<?PHP print $mobile_number ; ?>>
+<p>Enter your mobile number</p>
+
+<input type = 'text' name ='mobile_number' id="number" value=<?PHP print $mobile_number ; ?>>
 <input type = "submit" name = "submit1" value = "submit">
 </form>
 
