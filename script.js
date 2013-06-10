df8b659fa12fad2e146a7ea07a7438607eb39345
pranabjain <pranabjain24@gmail.com>
2013-06-10 18:55:43 +0530

    add script

---
+++ script.js
@@ -0,0 +1,13 @@
+
+<script>
+
+var Num = [];
+  Function datastore(){
+    
+    var textData=document.getElementById("number").value;
+    Num.push(textData);
+    if (textData==null || x==""){
+      alert("First number must be filled out");
+      return false;
+  }
+</script>
